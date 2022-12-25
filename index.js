b2c11be77a0c19cc5395e3d63ed88b10944221d0
TFMahabub <rjmahabub543@gmail.com>
2022-12-26 00:40:01 +0600

    products collection

--- index.js
+++ index.js
@@ -3,14 +3,73 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleWare-
 app.use(cors())
 app.use(express.json())
 
-app.get('/products/glocery', (req, res)=>{
-  
-})
+
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function Products(){
+  const productsCollection = client.db('easy-shop').collection('products')
+  try{
+    app.get('/products', async(req, res)=>{
+      const productCategories = req.query.product_category;
+      const query = {porductCategory: productCategories};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+  }
+  catch{
+
+  }
+  finally{
+
+  }
+}
+Products()
+.catch(err=>console.log(err, "products"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res)=>{
   res.send('easy shop server is running...')
