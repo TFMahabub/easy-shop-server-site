685d92d900322d0feff78d85ea38f5c2328b16ec
TFMahabub <rjmahabub543@gmail.com>
2022-12-27 14:46:13 +0600

    single product find

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleWare-
 app.use(cors())
@@ -24,6 +24,12 @@ async function Products(){
       const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    })
+    app.get('/products/product_details/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const product = await productsCollection.findOne(query)
+      res.send(product);
     })
   }
   catch{
